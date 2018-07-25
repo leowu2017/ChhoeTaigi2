76434e35dcdfa5d450206bd892b785fb3d73fb4c
Leo <leowu2017@gmail.com>
2018-07-26 01:25:47 +0800

    form submit

--- client/main.js
+++ client/main.js
@@ -1,4 +1,11 @@
 import { Template } from 'meteor/templating';
 
-//import './main.html';
 import '../lib/router.js';
+import './result.html';
+
+Template.home.events({
+  'submit #myForm'(event) {
+    const value = event.target.myText.value;
+    BlazeLayout.render('result', {'value': value});
+  }
+})
